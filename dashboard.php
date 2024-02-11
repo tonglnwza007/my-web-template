4fa71b4ac5288d37131d05b4601b76b56d52403e
ploythun <ploysrijundon@gmail.com>
2024-02-11 16:33:21 +0700

    yes

--- dashboard.php
+++ dashboard.php
@@ -110,9 +110,9 @@
                 echo "Error: " . $e->getMessage();
             }
         ?>
-                    <h1 class="display-5 fw-bold text-body-emphasis">Welcome, <?php echo $userData['username'] ?></h1>
+                    <h1 class="display-5 fw-bold text-body-emphasis">Welcome, <?php echo $userData['username']; ?></h1>
                     <div class="col-lg-6 mx-auto">
-                        <p class="lead mb-4">Email : <?php echo $userData['email'] ?></p>
+                        <p class="lead mb-4">Email : <?php echo $userData['email']; ?></p>
  
                     <a href="profile.php" class="btn btn-danger">edit your profile</a>
                         </div>
